bae1dd4fc5d561b0a4d347115f1d9fa822593594
vskode <vkather@gmail.com>
2022-09-16 11:32:05 +0100

    cleaned up training script

--- train_google.py
+++ train_google.py
@@ -1,41 +1,71 @@
-#%%
 import os
 import pandas as pd
 import numpy as np
-from pathlib import Path
 import tensorflow as tf
 import time
 import json
-
+import yaml
 from utils.tfrec import get_dataset, check_random_spectrogram
 from utils.google_funcs import GoogleMod
 
-params = {
-    "sr" : 10000,
-    "cntxt_wn_hop": 39124,
-    "cntxt_wn_sz": 39124,
-    "fft_window_length" : 2**11,
-    "n_freq_bins": 2**8,
-    "freq_cmpr": 'linear',
-    "fmin":  50,
-    "fmax":  1000,
-    "nr_noise_samples": 100,
-    "sequence_len" : 39124,
-    "fft_hop": 300,
-    "lr": 1e-3,
-}
+with open('humpzam/config.yml', 'r') as f:
+    config = yaml.safe_load(f)
+params = config['preproc']
+
+
 
 TFRECORDS_DIR = 'Daten/tfrecords*s_shift'
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-#%% init
-# time.sleep(2000)
 batch_size = 32
 epochs = 50
-# steps_per_epoch = 400
-rep = 1
 
+load_weights = False
+steps_per_epoch = False
+rep = 1
+good_file_size = 432
+poor_file_size = 236
+num_of_shifts = 5
+init_lr = 1e-2
+final_lr = 1e-5
+
+unfreezes = [2, 9, 15, 25]
+
+
+
+info_text = f"""Model run INFO:
+
+model: untrained model 
+dataset: good and poor data, 5 shifts from 0s - 2s
+lr: new lr settings
+comments:
+
+VARS:
+batch_size      = {batch_size}
+epochs          = {epochs}
+load_weights    = {load_weights}
+steps_per_epoch = {steps_per_epoch}
+rep             = {rep}
+good_file_size  = {good_file_size}
+poor_file_size  = {poor_file_size}
+num_of_shifts   = {num_of_shifts}
+init_lr         = {init_lr}
+final_lr        = {final_lr}
+unfreezes       = {unfreezes}
+"""
+
+
+
+
+#############################################################################
+#############################  RUN  #########################################
+#############################################################################
+
+
+
+time_start = time.strftime('%Y-%m-%d_%H', time.gmtime())
+dataset_size = (good_file_size + poor_file_size)*num_of_shifts
 
 train_files = tf.io.gfile.glob(f"{TFRECORDS_DIR}/train/*.tfrec")
 train_data = get_dataset(train_files, batch_size, AUTOTUNE = AUTOTUNE)
@@ -44,31 +74,32 @@ test_files = tf.io.gfile.glob(f"{TFRECORDS_DIR}/test/*.tfrec")
 test_data = get_dataset(test_files, batch_size, AUTOTUNE = AUTOTUNE)
 
 train_data = train_data.shuffle(50)
-check_random_spectrogram(train_files, dataset_size = 20000)
+check_random_spectrogram(train_files, dataset_size = dataset_size*batch_size)
 
-unfreezes = [2, 9, 15, 25]
-# lrs = np.linspace(1.4e-3, 2.5e-3, 5)
-lr = tf.keras.optimizers.schedules.ExponentialDecay(1e-2,
-                                        decay_steps = 1000,
-                                        decay_rate = 0.7,
-                                        staircase = True)
-for unfreeze in unfreezes:
-    # for lr in lrs:
+
+lr = tf.keras.optimizers.schedules.ExponentialDecay(init_lr,
+                                decay_steps = dataset_size,
+                                decay_rate = (final_lr/init_lr)**(1/epochs),
+                                staircase = True)
+for ind, unfreeze in enumerate(unfreezes):
         
-    params['lr'] = lr
+    config['model']['lr'] = lr
 
-    G = GoogleMod(params)
+
+    G = GoogleMod(config['model'])
     model = G.model
     for layer in model.layers[:-unfreeze]:
         layer.trainable = False
 
-    # model.load_weights(f'trainings/unfreeze_{unfreeze}_lr_exp/cp-0035.ckpt')
+    if load_weights:
+        model.load_weights(
+            f'trainings/unfreeze_{unfreeze}_lr_exp/cp-0035.ckpt')
 
-    #%% define training
-    checkpoint_path = f"trainings/unfreeze_{unfreeze}_" + \
+    checkpoint_path = f"trainings/{time_start}/unfreeze_{unfreeze}_" + \
                         f"lr_exp_all_data" + \
                         "/cp-{epoch:04d}.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
+    
 
     # Create a callback that saves the model's weights every 5 epochs
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -79,6 +110,10 @@ for unfreeze in unfreezes:
 
     # Save the weights using the `checkpoint_path` format
     model.save_weights(checkpoint_path.format(epoch=0))
+    
+    if ind == 0:
+        with open(f'trainings/{time_start}/training_info.txt', 'w') as f:
+            f.write(info_text)
 
     # Train the model with the new callback
     hist = model.fit(train_data, 
@@ -91,13 +126,11 @@ for unfreeze in unfreezes:
     result = hist.history
     avg_val_loss = np.mean(result['val_loss'][-int(len(result)/3):])
 
-    pd.DataFrame().to_csv(f"{checkpoint_dir}/res_2nd_run_{avg_val_loss:.2f}.csv")
+    pd.DataFrame().to_csv(f"{checkpoint_dir}/results_{avg_val_loss:.2f}.csv")
 
-    with open(f"{checkpoint_dir}/results_2nd_run.json", 'w') as f:
+    with open(f"{checkpoint_dir}/results.json", 'w') as f:
         json.dump(result, f)
 
-
-#%%
 
 
     
