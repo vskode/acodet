cb19fe6a71c9790b016c6b8a837a4f422da0260f
vskode <vkather@gmail.com>
2022-09-30 15:30:30 +0100

    added better folder structure when writing predictions

--- gen_prediction_annotations.py
+++ gen_prediction_annotations.py
@@ -1,4 +1,5 @@
 import yaml
+import time
 import numpy as np
 import pandas as pd
 import librosa as lb
@@ -11,27 +12,44 @@ with open('hbdet/hbdet/config.yml', 'r') as f:
 
 params = config['preproc']
 
-# file = Path('Daten/OneDrive_1_1-24-2022/channelA_2021-03-18_01-00-05.wav')
-# file = Path('/media/vincent/Expansion/Tolsta/2020/D8_Tolsta_wavs/335564853.200222210624.wav')
-file = Path('/home/vincent/Code/MA/generated_annotations/335564853.200222210624.wav')
-# file = Path('/home/vincent/Code/MA/Daten/googles_train_data/Hawaii_K_02_080506_183545.d20.x.wav')
-# file = Path('/media/vincent/Expansion/NOAA/NRS08_20162018_new20220711/NRS08_20160414_233332.wav')
+def get_files():
+    # file = Path('Daten/OneDrive_1_1-24-2022/channelA_2021-03-18_01-00-05.wav')
+    # file = Path('/media/vincent/Expansion/Tolsta/2020/D8_Tolsta_wavs/335564853.200222210624.wav')
+    # file = Path('/home/vincent/Code/MA/generated_annotations/335564853.200222210624.wav')
+    # file = Path('/mnt/c/Documents and Settings/sa01vk/Documents/NOAA/MELLINGER_NOVA-SCOTIA_200508_EmrldN/EmrldN-00000005-050821-153834.wav')
+    # file = Path('/home/vincent/Code/MA/Daten/googles_train_data/Hawaii_K_02_080506_183545.d20.x.wav')
+    # file = Path('/media/vincent/Expansion/NOAA/NRS08_20162018_new20220711/NRS08_20160414_233332.wav')
+    folder = Path('/mnt/e/MA/SAMOSAS_VINCENT')
+    return folder.glob('*_2kHz_March-April/2021-03-13_*/**/*.wav')
 
 # Create a new model instance
-G = GoogleMod(config['model'])
-model = G.model
+def get_model(model_checkpoint, untrained = False):
+    G = GoogleMod(config['model'])
+    model = G.model
 
-# model.load_weights('trainings/2022-09-24_19/unfreeze_15/cp-0092.ckpt')
-model.load_weights('models/google_humpback_model')
-
-
-def gen_raven_annotation(file, model, resample = True):
-    if not resample:
-        audio_flat, _ = lb.load(file, sr = params['sr'])
+    if untrained:
+        model.load_weights('models/google_humpback_model')
+        mod_label = 'untrained'
     else:
-        audio_flat, _ = lb.load(file, sr = 2000)
-        audio_flat = lb.resample(audio_flat, orig_sr = 2000, 
-                                 target_sr = params['sr'])
+        model_checkpoint = model_checkpoint
+        mod_label = str(Path(model_checkpoint).parent.parent.stem
+                        + Path(model_checkpoint).parent.stem.replace('unfreeze_', '_u')
+                        + Path(model_checkpoint).stem.replace('cp-0', '_cp'))
+        model.load_weights(f'trainings/{model_checkpoint}')
+    return model, mod_label
+
+def gen_raven_annotation(file, model, mod_label, time_start, resample = True):
+    try:
+        if not resample:
+            audio_flat, _ = lb.load(file, sr = params['sr'])
+        else:
+            audio_flat, _ = lb.load(file, sr = 2000)
+            audio_flat = lb.resample(audio_flat, orig_sr = 2000, 
+                                    target_sr = params['sr'])
+        if len(audio_flat) == 0: return
+    except:
+        print("File is corrputed and can't be loaded.")
+        return
 
     pred_len_samps = config['model']['pred_win_lim'] * params['cntxt_wn_sz']
     if len(audio_flat) > pred_len_samps:
@@ -76,9 +94,18 @@ def gen_raven_annotation(file, model, resample = True):
     annots.index  = np.arange(1, len(annots)+1)
     annots.index.name = 'Selection'
 
-    annots.to_csv(f'generated_annotations/{file.stem}_annot_untrained.txt', 
-                  sep='\t')
+    save_path = Path(f'generated_annotations/{time_start}/{file.parent.parent.stem}/'
+        f'{file.parent.stem}')
+    save_path.mkdir(exist_ok=True, parents=True)
+
+    annots.to_csv(save_path.joinpath(f'{file.stem}_annot_{mod_label}.txt'),
+                sep='\t')
     
     
 if __name__ == '__main__':
-    gen_raven_annotation(file, model, resample=True)
+    time_start = time.strftime('%Y-%m-%d_%H', time.gmtime())
+    files = get_files()
+    for file in files:
+        model, label = get_model('2022-09-26_16/unfreeze_15/cp-0094.ckpt', 
+                        untrained = False)
+        gen_raven_annotation(file, model, label, time_start, resample=True)
